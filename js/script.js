133c2d78a2e0bdc16fed2d9ac2031de417429963
Teron19 <vladmordasov0@gmail.com>
2018-11-28 00:31:27 +0200

    Slider fail

--- js/script.js
+++ js/script.js
@@ -9,3 +9,37 @@ menu.onclick = function myFunction() {
 	}
 }
 /*Menu*/ 
+/*Slider*/
+var sideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+
+	if (n > slides.lenght){
+		slideIndex = 1
+	}
+	if (n < 1){
+		slideIndex = slides.lenght
+	}
+	for (i = 0; i < slides.lenght; i++){
+		slides[i].style.display= "none";
+	}
+	for (i = 0; i < dost.lenght; i++){
+		dots[i].className = dots[i].className.replace("active","");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[sldeIndex-1].className+= " active";
+}
+ 
+
+/*Slider*/
